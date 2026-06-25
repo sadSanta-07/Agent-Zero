73e2854259b28e1dbf943ce6b0682fb0777ecf6b
pechkas <iffcogc34@gmail.com>
2026-06-25 11:48:33 +0530

    refactor: clean up firebase.ts by removing unnecessary comments and ensuring consistent scope addition

--- src/firebase.ts
+++ src/firebase.ts
@@ -3,18 +3,16 @@ import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from 'firebase/
 import { getFirestore } from 'firebase/firestore';
 import firebaseConfig from '../firebase-applet-config.json';
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-// Initialize Firestore and Auth
 export const db = getFirestore(app);
 export const isFirestoreEnabled = true;
-
 export const auth = getAuth(app);
 export const googleProvider = new GoogleAuthProvider();
 
 googleProvider.addScope('https://www.googleapis.com/auth/gmail.send');
 googleProvider.addScope('https://www.googleapis.com/auth/gmail.readonly');
 googleProvider.addScope('https://www.googleapis.com/auth/calendar.events');
+googleProvider.addScope('https://www.googleapis.com/auth/documents');
 
 export { signInWithPopup, signOut };
